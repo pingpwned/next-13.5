0c2b67467a885c2b4a201c213f9848404d9b1437
pingpwned <pingpwned@nblx.me>
2023-09-30 12:12:37 +0200

    fix header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ export const Header = () => {
   const handleClick = () => setOpen(!isOpen);
 
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-900">
+    <nav className="border-gray-200">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="/" className="flex items-center text-gray-900">
           {/* <img src="next.svg" className="h-8 mr-3" alt="Logo" /> */}
@@ -57,7 +57,7 @@ export const Header = () => {
             !isOpen ? "hidden" : "w-full"
           } md:block md:w-auto" id="navbar-default`}
         >
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 dark:border-gray-700">
             <li>
               <Link
                 href={usePathname()}
