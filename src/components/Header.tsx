fb7d60caea9d88bb0ae1d2ba4357bc08f85401a5
pingpwned <pingpwned@nblx.me>
2023-10-10 13:15:06 +0200

    fix header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -51,11 +51,7 @@ export const Header = () => {
             />
           </svg>
         </button>
-        <div
-          className={`${
-            !isOpen ? "hidden" : "w-full"
-          } md:block md:w-auto" id="navbar-default`}
-        >
+        <div className={`${!isOpen ? "hidden" : "w-full"} md:block md:w-auto`}>
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 dark:border-gray-700">
             <li>
               <Link
